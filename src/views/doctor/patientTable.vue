a16084eb53bacf3c29569e671ee9e7ff12bd702e
t0ugh <1243384249@qq.com>
2019-06-18 02:10:20 +0800

    Feat:新增患者列表card

---
+++ src/views/doctor/patientTable.vue
@@ -0,0 +1,40 @@
+<template>
+        <b-card header="患者列表">
+          <b-table :items="m_items" :fields="m_fields">
+            <template slot="status" slot-scope="data">
+              <b-badge :variant="data.item.status">{{data.item.status}}</b-badge>
+            </template>
+            <template slot="选择" slot-scope="row">
+              <b-button variant="outline-primary" size="sm" @click="selectUser(row.index)" class="mr-2">
+                选择
+              </b-button>
+            </template>
+          </b-table>
+        </b-card>
+</template>
+<script>
+    export default {
+      props:['initialFields', 'initialItems'],
+      data() {
+        return {
+          innerFields: ['选择'],
+          m_items: this.initialItems
+        }
+      },
+      computed: {
+          m_fields :function(){
+            return this.initialFields.concat(this.innerFields);
+          }
+      },
+      methods:{
+        selectUser (key) {
+          console.log(key);
+          this.$emit('select_user', key)
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
