84591a78a44072eb3deba028ced361c6f78e5120
t0ugh <1243384249@qq.com>
2019-06-19 09:39:43 +0800

    Fix:添加本地配置防止污染全局文件

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,9 @@
+import localConfig from "./config.local.js"
+
 let baseUrl = '';
 
 if (process.env.NODE_ENV === 'development') {
-    baseUrl = '/api';
+    baseUrl = localConfig.developUrl;
 
 } else if (process.env.NODE_ENV === 'production') {
     baseUrl = 'https://120.79.88.98:8080/hoso';
