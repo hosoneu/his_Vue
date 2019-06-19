56623efc114646910992946dce8a019e5e5c8e0a
t0ugh <1243384249@qq.com>
2019-06-19 10:09:22 +0800

    Fix:修复配置文件中动态加载本地配置，太简单了就不分枝了

--- src/config/env.js
+++ src/config/env.js
@@ -1,9 +1,9 @@
-import localConfig from "./config.local.js"
-
+// import localConfig from "./config.local"
 let baseUrl = '';
 
 if (process.env.NODE_ENV === 'development') {
-    baseUrl = localConfig.developUrl;
+    const localConfig = require("./config.local");
+    baseUrl = localConfig.developUrl
 
 } else if (process.env.NODE_ENV === 'production') {
     baseUrl = 'https://120.79.88.98:8080/hoso';
