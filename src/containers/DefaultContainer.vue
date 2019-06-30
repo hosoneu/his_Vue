523d972e54ee63f3364cda7293445af350f515d1
t0ugh <1243384249@qq.com>
2019-06-30 13:51:12 +0800

    Feat:图标生成

--- src/containers/DefaultContainer.vue
+++ src/containers/DefaultContainer.vue
@@ -2,7 +2,7 @@
   <div class="app">
     <AppHeader fixed>
       <b-link class="navbar-brand" to="#">
-        <img class="navbar-brand-full" src="img/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
+        <img class="navbar-brand-full" src="img/brand/logo.svg" width="90" height="30" alt="CoreUI Logo">
         <img class="navbar-brand-minimized" src="img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
       </b-link>
       <b-navbar-nav class="d-md-down-none">
