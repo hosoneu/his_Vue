fe671fd73aa337e155799b42cefb95c1c826dc0d
t0ugh <1243384249@qq.com>
2019-06-30 14:25:54 +0800

    Feat:store当前用户

--- src/store/common.js
+++ src/store/common.js
@@ -1,11 +1,15 @@
 export default {
   namespaced:true,
   state:{
-    curr_user_type:'home'
+    curr_user_type:'home',
+    curr_user:null
   },
   mutations:{
     set_curr_user_type(state, user_type){
       state.curr_user_type = user_type
+    },
+    set_curr_user(state, user) {
+      state.curr_user = user
     }
   }
 }
