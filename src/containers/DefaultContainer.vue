717a601f901cca5bd1a2f8b2be7f8bc76b5f0eaa
t0ugh <1243384249@qq.com>
2019-07-02 15:22:10 +0800

    Feat:引入下拉组件

--- src/containers/DefaultContainer.vue
+++ src/containers/DefaultContainer.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="app">
-    <b-navbar toggleable="sm" type="light" tag="header" variant="light" fixed>
-      <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+    <b-navbar toggleable="sm" type="light" tag="header" variant="light" class="app-header" fixed>
+      <b-navbar-nav class="md-auto">
+        <DefaultDropdownNav/>
+      </b-navbar-nav>
       <b-link class="navbar-brand" to="#">
         <img class="navbar-brand-full" src="img/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
-        <img class="navbar-brand-minimized" src="img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">
+        <!--<img class="navbar-brand-minimized" src="img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo">-->
       </b-link>
-      <b-collapse id="nav-text-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
+        <b-navbar-nav class="d-md-down-none">
           <b-nav-item v-for="(item, index) in nav" :key ="index" class="px-3" :to="item.url">
             {{item.name}}
           </b-nav-item>
         </b-navbar-nav>
-        <b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
           <DefaultHeaderDropdownAccnt/>
         </b-navbar-nav>
-      </b-collapse>
     </b-navbar>
     <div class="app-body">
       <main class="main">
@@ -47,11 +47,13 @@ import nav from '../nav'
 import { Header as AppHeader, SidebarToggler, Sidebar as AppSidebar, SidebarFooter, SidebarForm, SidebarHeader, SidebarMinimizer, SidebarNav, Aside as AppAside, AsideToggler, Footer as TheFooter, Breadcrumb } from '@coreui/vue'
 import DefaultAside from './DefaultAside'
 import DefaultHeaderDropdownAccnt from './DefaultHeaderDropdownAccnt'
+import DefaultDropdownNav from "./DefaultDropdownNav"
 import {mapState} from 'vuex'
 
 export default {
   name: 'DefaultContainer',
   components: {
+    DefaultDropdownNav,
     AsideToggler,
     AppHeader,
     AppSidebar,
