a8f75c79ebc1371ab277b7192033de5aae64355d
t0ugh <1243384249@qq.com>
2019-07-05 01:42:23 +0800

    Fix:添加缓存解决刷新丢数据

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,14 @@
 <script>
 export default {
   name: 'app',
+  created(){
+    if(sessionStorage.getItem("store")){
+      this.$store.replaceState(Object.assign({}, this.$store.state,JSON.parse(sessionStorage.getItem("store"))))
+    }
+    window.addEventListener("beforeunload",()=>{
+      sessionStorage.setItem("store",JSON.stringify(this.$store.state))
+    })
+  },
   provide () {
     return {
       reload: this.reload
